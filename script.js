0812f2d9fe31036715ce91f435a178bbb6f7b8f4
kams <kevinmora@gmail.com>
2025-03-29 10:16:54 -0600

    cambios en el css y inicio del js

---
+++ script.js
@@ -0,0 +1,4 @@
+const billInput = document.querySelector(".bill-input");
+const peopleInput = document.querySelector(".people-input");
+const tipPerson = document.getElementById(".tip-amount");
+const totalPerPerson = document.getElementById("total-amount");
