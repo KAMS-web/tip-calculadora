adbdcc904daf5cd4ee0e4e30dee34b61c9d6be24
kams <kevinmora@gmail.com>
2025-03-29 10:26:04 -0600

    entradas para bill input y people inpu en js

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const billInput = document.querySelector(".bill-input");
 const peopleInput = document.querySelector(".people-input");
 const tipPerson = document.getElementById(".tip-amount");
 const totalPerPerson = document.getElementById("total-amount");
+
+billInput.addEventListener("input", billInputFun);
+peopleInput.addEventListener("input", peopleInputFun);
+
+billInput.value = "0.0";
+peopleInput.value = "1";
+tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+totalPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+
+let billvalue = 0.0;
+let peoplevalue = 1;
+
+function billInputFun(){
+    billvalue = parseFloat(billInput.value);
+    console.log(billvalue);
+}
+
+function peopleInputFun(){
+    peoplevalue = parseFloat(peopleInput.value);
+}
